62361c236caaa928492d1a8226f8b2b245bc868e
Martin Berthold <martin.berthold@akquinet.de>
2017-04-07 15:39:34 +0200

    Debugging

--- src/QlikConnectorPSExecute/ScriptCode.cs
+++ src/QlikConnectorPSExecute/ScriptCode.cs
@@ -115,6 +115,8 @@
                 if (String.IsNullOrEmpty(TableName))
                     TableName = ExecuteName;
 
+                logger.Warn("Step1");
+
                 return true;
             }
             catch (Exception ex)
